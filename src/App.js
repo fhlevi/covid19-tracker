eddfc2d64b889c168a8e4e61593fa3bce4a0bdf8
Levi <faisal@sakoo.id>
2021-01-30 14:33:21 +0700

    add google analystic in code

--- src/App.js
+++ src/App.js
@@ -9,6 +9,28 @@ const TheLayout = React.lazy(() => import('./components/TheLayout.js'))
 class App extends Component {
   componentDidMount(){
     document.title = 'Covid Tracking'
+    this.setGoogleAnalytic()
+  }
+  setGoogleAnalytic(){
+    (function(){
+        const s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+        s1.async=true;
+        s1.src='https://www.googletagmanager.com/gtag/js?id=G-WR966Y57H4';
+        s0.parentNode.insertBefore(s1,s0);
+    })();
+
+    const script = document.createElement("script");
+    script.innerHTML = this.analytics('G-WR966Y57H4');
+    document.head.appendChild(script);
+  }
+  analytics(id){
+    return `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', '${id}');
+    `
   }
   render() {
     return (
